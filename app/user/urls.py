d6704d093b28c412388f502f9c0d8f5bd33cdadb
nicolas <nicolas.alahmar@sayeghonline.com>
2024-09-11 13:20:24 +0300

    12 - Build user API

---
+++ app/user/urls.py
@@ -0,0 +1,12 @@
+"""url mapping for the users API"""
+from user import views
+from django.urls import path
+
+
+app_name = 'user'
+
+urlpatterns = [
+    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('me/', views.ManageUserView.as_view(), name='me'),
+]
